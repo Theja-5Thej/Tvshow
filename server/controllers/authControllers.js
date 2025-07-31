0d96f312d825dab9eebf5c51c8334fdc47e8d020
Theja <gaddamtheja@gmail.com>
2025-07-31 22:15:49 +0530

    updated

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -20,7 +20,7 @@ exports.register = async (req, res) => {
     });
 
     if (existingUser) {
-      return res.status(400).json({ error: 'Email or username already in use' });
+      return res.status(400).json({ message: 'Email or username already in use' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -36,10 +36,10 @@ exports.register = async (req, res) => {
       JWT_SECRET,
       { expiresIn: '1h' }
     );
-
+    const { password: _pw, ...safeUser } = newUser.get({ plain: true });
     res.status(201).json({
       message: 'User registered successfully',
-      token
+      data:{token,safeUser}
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -53,15 +53,15 @@ exports.login = async (req, res) => {
 
     const user = await User.findOne({ where: { email } });
 
-    if (!user) return res.status(400).json({ error: 'Invalid email or password' });
+    if (!user) return res.status(400).json({ message: 'Invalid email or password' });
 
     const match = await bcrypt.compare(password, user.password);
 
-    if (!match) return res.status(400).json({ error: 'Invalid email or password' });
+    if (!match) return res.status(400).json({ message: 'Invalid email or password' });
 
     const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '5d' });
     const { password: _pw, ...safeUser } = user.get({ plain: true });
-    res.json({ token, safeUser });
+    res.json({ token,  data:{token,safeUser} });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
